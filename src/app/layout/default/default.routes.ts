da8af6f6d906d870c6e49f72edd05599c6c6b622
martins86 <martins86@gmail.com>
2024-05-27 23:44:38 +0000

    fix: rotas do layout default

--- src/app/layout/default/default.routes.ts
+++ src/app/layout/default/default.routes.ts
@@ -1,40 +1,45 @@
 import { Routes } from '@angular/router';
 
+import { DefaultComponent } from './default.component';
+
 export const LAYOUT_DEFAULT: Routes = [
   {
-    path: 'login',
-    loadChildren: () =>
-      import('@pages/authentication/login/login.routes').then(
-        (m) => m.LOGIN_ROUTE,
-      ),
-  },
-  {
-    path: 'logout',
-    loadChildren: () =>
-      import('@pages/authentication/logout/logout.routes').then(
-        (m) => m.LOGOUT_ROUTE,
-      ),
-  },
-  {
-    path: 'registration',
-    loadChildren: () =>
-      import('@pages/authentication/registration/registration.routes').then(
-        (m) => m.REGISTRATION_ROUTE,
-      ),
-  },
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('@pages/home/home.routes').then((m) => m.HOME_ROUTE),
-  },
-  {
-    path: '**',
-    loadChildren: () =>
-      import('@pages/not-found/not-found.routes').then((m) => m.NOTFOUND_ROUTE),
-  },
-  {
     path: '',
-    redirectTo: '/login',
-    pathMatch: 'full',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('@pages/home/home.routes').then((m) => m.HOME_ROUTE),
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('@pages/authentication/login/login.routes').then(
+            (m) => m.LOGIN_ROUTE,
+          ),
+      },
+      {
+        path: 'logout',
+        loadChildren: () =>
+          import('@pages/authentication/logout/logout.routes').then(
+            (m) => m.LOGOUT_ROUTE,
+          ),
+      },
+      {
+        path: 'registration',
+        loadChildren: () =>
+          import('@pages/authentication/registration/registration.routes').then(
+            (m) => m.REGISTRATION_ROUTE,
+          ),
+      },
+      {
+        path: '**',
+        loadChildren: () =>
+          import('@pages/not-found/not-found.routes').then(
+            (m) => m.NOTFOUND_ROUTE,
+          ),
+      },
+    ],
   },
 ];
