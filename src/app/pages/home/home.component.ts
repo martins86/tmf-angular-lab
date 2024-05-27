a94676a6690faec45cecb13608c020480d44586c
martins86 <martins86@gmail.com>
2024-05-27 22:45:38 +0000

    feat: acionando componentes layout home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+import { BottomBarComponent } from '@layout/default/components/bottom-bar/bottom-bar.component';
+import { LeftBarComponent } from '@layout/default/components/left-bar/left-bar.component';
+import { LoadingComponent } from '@layout/default/components/loading/loading.component';
+import { TopBarComponent } from '@layout/default/components/top-bar/top-bar.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    TopBarComponent,
+    LeftBarComponent,
+    BottomBarComponent,
+    LoadingComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
