0e3c5491d6696c74a8bff1523f5d8f275cc4c97b
martins86 <martins86@gmail.com>
2024-05-28 00:19:06 +0000

    feat: adicionando providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { TitleStrategy, provideRouter } from '@angular/router';
 
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TemplatePageTitleStrategy } from '@layout/default/default.routes';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+    provideAnimationsAsync(),
+  ],
 };
