96b0091783cfecfc2cf2b0c25437db001182ce12
martins86 <martins86@gmail.com>
2024-05-28 23:01:12 +0000

    feat: ajustando layout default

--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component';
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TopBarComponent,
+    LeftBarComponent,
+    BottomBarComponent,
+    LoadingComponent,
+  ],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss',
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
 
-import { BottomBarComponent } from '@layout/default/components/bottom-bar/bottom-bar.component';
-import { LeftBarComponent } from '@layout/default/components/left-bar/left-bar.component';
-import { LoadingComponent } from '@layout/default/components/loading/loading.component';
-import { TopBarComponent } from '@layout/default/components/top-bar/top-bar.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    TopBarComponent,
-    LeftBarComponent,
-    BottomBarComponent,
-    LoadingComponent,
-  ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
