a448491c2155be9569543904c13785c8ba1e1cc8
martins86 <martins86@gmail.com>
2024-05-28 22:55:28 +0000

    feat: iniciando footer

--- src/app/layout/default/components/bottom-bar/bottom-bar.component.ts
+++ src/app/layout/default/components/bottom-bar/bottom-bar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-bottom-bar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './bottom-bar.component.html',
   styleUrl: './bottom-bar.component.scss',
 })
-export class BottomBarComponent {}
+export class BottomBarComponent {
+  currentYear: string;
+  constructor() {
+    this.currentYear = new Date().getFullYear().toString();
+  }
+}
