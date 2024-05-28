44b348a0bc6017b57a583f94f58f87e8ea80e714
martins86 <martins86@gmail.com>
2024-05-28 01:24:59 +0000

    feat: adicionando top bar com o botão menu

--- src/app/layout/default/components/top-bar/top-bar.component.ts
+++ src/app/layout/default/components/top-bar/top-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AnimatedMenuButtonComponent } from '@shared/components/animated-menu-button/animated-menu-button.component';
+
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, AnimatedMenuButtonComponent],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
