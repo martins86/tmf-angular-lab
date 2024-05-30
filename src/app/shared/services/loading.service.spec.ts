41e6d99a2daaa2e95fc17b9fc3052a53f381b126
martins86 <martins86@gmail.com>
2024-05-30 20:16:58 +0000

    feat: iniciando service do loading

---
+++ src/app/shared/services/loading.service.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from '@angular/core/testing';
+
+import { LoadingService } from './loading.service';
+
+describe('Testes do LoadingService', () => {
+  let service: LoadingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LoadingService);
+  });
+
+  it('Deve inicializar o LoadingService', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Testando o setSpinner', () => {
+    it('Deve setar o boolean recebido no loading', () => {
+      // Arrange
+      service['loading'] = true;
+
+      // Act
+      service.setSpinner(false);
+
+      // Assert
+      expect(service['loading']).toBeFalse();
+    });
+  });
+
+  describe('Testando o getSpinner', () => {
+    it('Deve retornar o boolean do loading', () => {
+      // Arrange
+      service['loading'] = true;
+
+      // Act
+      const result = service.getSpinner();
+
+      // Assert
+      expect(result).toBeTrue();
+    });
+  });
+});
---
+++ src/app/shared/services/loading.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  private loading: boolean = false;
+
+  setSpinner(loading: boolean): void {
+    this.loading = loading;
+  }
+
+  getSpinner(): boolean {
+    return this.loading;
+  }
+}
