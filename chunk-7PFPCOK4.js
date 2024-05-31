c1fd41a21dbee7c4c5dfbb9305283c52d7c44c42
martins86 <martins86@gmail.com>
2024-05-31 04:39:31 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/martins86/tmf-angular-lab/commit/841958c9e53d67cb87d5cf616e3477da0928c612

--- chunk-7PFPCOK4.js
+++
@@ -1,1 +0,0 @@
-import{h as a,i as b}from"./chunk-TCOZHTD7.js";import"./chunk-7XHK4FGP.js";import"./chunk-73NTLZLP.js";export{a as LAYOUT_DEFAULT,b as TemplatePageTitleStrategy};
---
+++ chunk-GOOXXBHD.js
@@ -0,0 +1,1 @@
+import{h as a,i as b}from"./chunk-EDWTF27H.js";import"./chunk-7XHK4FGP.js";import"./chunk-73NTLZLP.js";export{a as LAYOUT_DEFAULT,b as TemplatePageTitleStrategy};
